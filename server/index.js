67c5a51e4c9917afad4f6116cb6108efa75e0cb2
Heui Park <hpark41@wgu.edu>
2021-04-05 05:10:36 -0700

    Working up to Exercise 4.2

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const Blog = require('./models/blog')
+const BlogRouter = require('./routes/blogs')
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs',BlogRouter)
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
